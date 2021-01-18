91cc700320a22f1d42ba4d04c5065e3f0fa8fa71
Andrei <vdsekb@gmail.com>
2021-01-18 23:27:42 +0500

    ДЗ1. Программа выводящая "Имя" пользователя и "Дату"

---
+++ Base/DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как к вам можно обращаться?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет,  {name}, сегодня [{DateTime.Today.ToShortDateString()}].");
+            Console.ReadLine();
+        }
+    }
+}
