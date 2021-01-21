44fc0d17d828339ab50d582bff404f6431881c11
Andrei <vdsekb@gmail.com>
2021-01-21 22:25:11 +0500

    ДЗ2 Первые четыре задания

---
+++ Base/ConsoleApp1/Task4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Для полного закрепления понимания простых типов найдите любой чек,
+            // либо фотографию этого чека в интернете и схематично нарисуйте его в консоли, 
+            //только за место динамических, по вашему мнению, данных (это может быть дата, название магазина, 
+            //сумма покупок) подставляйте переменные, которые были заранее заготовлены до вывода на консоль.
+
+            string OOO = "ИП Рога и копыта";
+            string Magazin = "Магазин Звезда";
+            int index = 620050;
+            string street = "Екатерибург, Ленина 1";
+            string terminal = "Терминал";
+            int numberterminal = 27670101;
+            string chek = "Чек";
+            int numberchek = 6400;
+            string result = "Итог операции";
+            string resultpayments = "Оплачено";
+            string sum = "Сумма";
+            double price = 881.54;
+            string valuta = "RUB";
+            string nimbersilki = "Номер ссылки RRN";
+            long RRN = 1011010101010101010;
+            string date = "Дата";
+            DateTime numberdate = new DateTime(2020, 8, 4, 16, 23, 42); 
+
+
+            Console.WriteLine(OOO);
+            Console.WriteLine(Magazin);
+            Console.WriteLine($"{index}, {street}");
+            Console.WriteLine($"{terminal}: {numberterminal}");
+            Console.WriteLine($"{chek}: {numberchek}");
+            Console.WriteLine($"{result}: {resultpayments}");
+            Console.WriteLine($"{sum}: {price} {valuta}");
+            Console.WriteLine($"{nimbersilki}: {RRN}");
+            Console.WriteLine($"{date}: {numberdate}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Base/Job2/Task2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job2
+{
+    class Task2
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            //Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+
+
+            Console.WriteLine("Укажите номер текущего месяца");
+            string month = Console.ReadLine();
+            int i = Convert.ToInt32(month);
+            if (i == 1)
+            {
+                Console.WriteLine("Название месяца: Январь");
+                Console.ReadLine();
+            }
+            else if (i == 2)
+            {
+                Console.WriteLine("Название месяца: Февраль");
+                Console.ReadLine();
+            }
+            else if (i == 3)
+            {
+                Console.WriteLine("Название месяца: Март");
+                Console.ReadLine();
+            }
+            else if (i == 4)
+            {
+                Console.WriteLine("Название месяца: Апрель");
+                Console.ReadLine();
+            }
+            else if (i == 5)
+            {
+                Console.WriteLine("Название месяца: Май");
+                Console.ReadLine();
+            }
+            else if (i == 6)
+            {
+                Console.WriteLine("Название месяца: Июнь");
+                Console.ReadLine();
+            }
+            else if (i == 7)
+            {
+                Console.WriteLine("Название месяца: Июль");
+                Console.ReadLine();
+            }
+            else if (i == 8)
+            {
+                Console.WriteLine("Название месяца: Август");
+                Console.ReadLine();
+            }
+            else if (i == 9)
+            {
+                Console.WriteLine("Название месяца: Сентябрь");
+                Console.ReadLine();
+            }
+            else if (i == 10)
+            {
+                Console.WriteLine("Название месяца: Октябрь");
+                Console.ReadLine();
+            }
+            else if (i == 11)
+            {
+                Console.WriteLine("Название месяца: Ноябрь");
+                Console.ReadLine();
+            }
+            else if (i == 12)
+            {
+                Console.WriteLine("Название месяца: Декабрь");
+                Console.ReadLine();
+            }
+        }
+    }
+}
