8fa992c89afebb5014a42198068e5108fa9d8e2a
Andrei <vdsekb@gmail.com>
2021-01-25 20:42:37 +0500

    ДЗ3 - 1-4 задание

--- Base/DZ3/Task3/DZ3-Task3/DZ3-Task3/Program.cs
+++ Base/DZ3/Task3/DZ3-Task3/DZ3-Task3/Program.cs
@@ -10,10 +10,34 @@ namespace DZ3_Task3
     {
         static void Main(string[] args)
         {
-            string s;
 
-            while ((s = Console.ReadLine()) != null)
-                Console.WriteLine(new string(s.Reverse().ToArray()));
+            //Написать программу, выводящую введенную пользователем строку в обратном порядке (olleH вместо Hello).
+
+
+            //Варинт первый
+
+            //string s;
+
+            //while ((s = Console.ReadLine()) != null)
+            //    Console.WriteLine(new string(s.Reverse().ToArray()));
+
+
+            //Варинт второй
+
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            char[] TextRev = text.Reverse().ToArray();
+         
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i >= 0)
+                {
+
+                    System.Console.Write($"{TextRev[i]}");
+                    Console.ReadLine();
+                }
+            }
         }
-        }
+    }
 }
---
+++ Base/DZ3/Task4/DZ3-Task4/DZ3-Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ3_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //«Морской бой» — вывести на экран массив 10х10, состоящий из символов X и O, 
+            //где Х — элементы кораблей, а О — свободные клетки.
+
+
+            char[,] seabatle = new char[5, 5];
+            char element1 = 'Х';
+            char element2 = 'О';
+
+            for (int i = 0; i < seabatle.GetLength(0); i++)
+            {
+                for (int j = 0; j < seabatle.GetLength(1); j++)
+                {
+                    if (seabatle[i, j] == seabatle[i, i])
+                    {
+                        seabatle[i, j] = element1;
+                    }
+                    else
+                        seabatle[i, j] = element2;
+
+                    System.Console.Write(seabatle[i, j]);
+
+
+                }
+                System.Console.WriteLine();
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
