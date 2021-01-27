4f9d6a5235b6c7331335cc8ba60bdb53522e44a9
Andrei <vdsekb@gmail.com>
2021-01-28 00:00:46 +0500

    dz4 - 1,2 task

---
+++ Base/DZ4/Task1/DZ4-Task1/DZ4-Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4_Task1
+{
+    class Program
+    {
+
+        //Написать метод GetFullName(string firstName, string lastName, string patronymic), 
+        //принимающий на вход ФИО в разных аргументах и возвращающий объединённую строку с ФИО.
+        //Используя метод, написать программу, выводящую в консоль 3–4 разных ФИО.
+
+        static void Main(string[] args)
+        {
+
+            string name1 = FullName();
+            Console.ReadLine();
+
+        }
+
+        static string GetFullName()
+            {
+            // Тело запроса ФИО
+            Console.WriteLine("Введите фамилию");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Введите имя");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Введите Отчество");
+            string patronymic = Console.ReadLine();
+
+            // Полный вывод ФИО
+            Console.WriteLine($"Привет, {lastName} {firstname} {patronymic}");
+
+            return patronymic;
+            }
+
+
+
+        static string FullName()
+
+        {
+            // Цикл запроса ФИО из 4, удобно выставить нужное значение например 3(как указано в описании задачи)
+
+            for (int i = 0; i < 3; i++)
+            {
+                string name1 = GetFullName();
+            }
+            
+            // Конвертация для вывода return 0.
+            int q = 0;
+            string s1 = q.ToString();
+            return s1;
+
+        }
+    }
+}
---
+++ Base/DZ4/Task2/DZ4-Task2/DZ4-Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ4_Task2
+{
+    class Program
+    {
+        //Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, 
+        //и возвращающую число — сумму всех чисел в строке.
+        //Ввести данные с клавиатуры и вывести результат на экран.
+
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Введите набор чисел через пробел");
+            string text1 = Console.ReadLine();
+            Console.WriteLine($"Сумма чисел равна: {Sum(text1)}");
+            Console.ReadLine();
+        }
+
+
+        static int Sum(string text1)
+        {
+            return text1.Split(new char[] { ' ', }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Sum();
+        }
+
+
+    }
+}
+
+
