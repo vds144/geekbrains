4ef5ae632ca252f0cccdfd7f2b63461e7cfb8473
Andrei <vdsekb@gmail.com>
2021-02-03 11:44:27 +0500

    DZ5-Task5

---
+++ Base/DZ5/Task5.1/Task5.1/ToDo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5._1
+{
+    class ToDo
+    {
+        public int number;
+
+        public string Title{ get; set; }
+        public bool IsDone { get; set; }
+        public int Number { get; set; }
+
+        public ToDo(string title, bool isDone, int number)
+        {
+
+             Title = title;
+             IsDone = isDone;
+             Number = number;
+
+
+        }
+
+        public ToDo()
+        {
+
+        }
+}
+}
---
+++ Base/DZ5/Task5/Task5/TaskJob.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class TaskJob
+    {
+
+        public static string task { get; internal set; }
+
+        public TaskJob()
+        {
+
+            Console.WriteLine("Введит описание задачи");
+            string task = Console.ReadLine();
+            Console.WriteLine("Введите 1 в процессе, 0 выполнено, 2 ожидание");
+            string process = Console.ReadLine();
+            int i = Convert.ToInt32(process);
+
+            if (i == 1)
+            {
+                string progress = process;
+                Console.WriteLine($"Статус задачи {progress} - в процессе");
+                Console.ReadLine();
+            }
+            if (i == 0)
+            {
+                string done = process;
+                Console.WriteLine($"Статус задачи {done} - выполнено [x]");
+                Console.ReadLine();
+            }
+
+            if (i == 2)
+            {
+                string ojidanie = process;
+                Console.WriteLine($"Статус задачи {ojidanie} - Ожидание");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
