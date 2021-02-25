4951f107817c6284c53172142c83bc4cf44256c4
Andrei <vdsekb@gmail.com>
2021-02-25 19:30:20 +0500

    дз2

---
+++ Algoritm/DZ2/Task1/Task1/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrainsTests
+{
+    public class Item<T>
+    {
+        private T data = default(T);
+
+        public T Value
+        {
+            get => data;
+            set { if (value != null) data = value; else throw new ArgumentNullException(nameof(value)); }
+
+        }
+
+        public Item<T> NextItem { get; set; }
+
+        public Item(T data)
+        {
+         Value = data;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ Algoritm/DZ2/Task1/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBrainsTests
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var duplexList = new DuplexLinkedList<int>();
+            Console.WriteLine("добавляет новый элемент списка ");
+            duplexList.AddNode(1);
+            duplexList.AddNode(2);
+            duplexList.AddNode(3);
+            duplexList.AddNode(4);
+            duplexList.AddNode(5);
+
+            foreach (var item in duplexList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("возвращает количество элементов в списке");
+
+            duplexList.GetCount(1);
+
+            Console.WriteLine("удаляет элемент по порядковому номеру");
+
+            duplexList.RemoveNode(3);
+
+            foreach (var item in duplexList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("удаляет указанный элемент");
+            
+
+            duplexList.RemoveNode(2);
+            foreach (var item in duplexList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
+
+
